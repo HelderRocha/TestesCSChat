6584d94fa725333094f9adb807002f2ef497ff7d
Gustavo Caruso <gugacaruso@users.noreply.github.com>
2017-06-29 22:50:06 -0300

    Updating file
     Implemented a few auxiliary methods. Also, the tests for entering and creating a room. Need to adjust a few things when asserting, though.

--- src/teste/CSChat.java
+++ src/teste/CSChat.java
@@ -1,11 +1,14 @@
 package teste;
+
 import java.io.File;
 
+import static org.junit.Assert.assertTrue;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.Date;
 
 public class CSChat {
 	
@@ -14,9 +17,7 @@ public class CSChat {
 	private String URL;
 
 	public static void main(String[] args) {
-
         CSChat chat = new CSChat("http://localhost:1976/?templateset=dark");
-        
         chat.Mensagem(1);
 	}
 	
@@ -24,12 +25,14 @@ public class CSChat {
 		if (this.chrome1 != null)
 			return;
 		this.chrome1 = new ChromeDriver();
+		this.chrome1.get(this.URL);
 	}
 	
 	public void StartChrome2() {
 		if (this.chrome2 != null)
 			return;
 		this.chrome2 = new ChromeDriver();
+		this.chrome2.get(this.URL);
 	}
 	
 	CSChat(String chatURL) {
@@ -38,21 +41,20 @@ public class CSChat {
         System.setProperty("webdriver.chrome.driver", chromedriver.getAbsolutePath());
 
 		this.URL = chatURL;
-		this.chrome1 = new ChromeDriver();
-		//this.chrome2 = new ChromeDriver();
+		StartChrome1();
 	}
 	
 	public WebDriver GetDriver(int ind) {
 		switch (ind) {
-		case 1: 
-			StartChrome1();
-			return this.chrome1;
-		case 2:
-			StartChrome2();
-			return this.chrome2;
-		default:
-			StartChrome1();
-			return this.chrome1;
+			case 1: 
+				StartChrome1();
+				return this.chrome1;
+			case 2:
+				StartChrome2();
+				return this.chrome2;
+			default:
+				StartChrome1();
+				return this.chrome1;
 		}
 	}
 	
@@ -70,16 +72,75 @@ public class CSChat {
 
 	public void Mensagem(int ind) {
 		WebDriver driver = GetDriver(ind);
-		driver.get(URL);
 		Login(ind);
 		Login(ind);
 	}
 	
-	public void CriarSala() {
-		
+	public String CriarSala(int ind) {
+		String uniqueRoomName = "Room" + new Date().toString();
+		String createRoomCommand = "/j " + uniqueRoomName;
+		WebDriver driver = GetDriver(ind);
+
+		Login(ind);
+		sendCommandViaChat(driver, createRoomCommand);
+
+		return uniqueRoomName;
 	}
 	
-	public void EntrarSala() {
+	public String EntrarSala() {
+		Login(1);
+		Login(2);
+		String roomName = CriarSala(1);
+		String enterExistingRoomCommand = "/j " + roomName;
+		WebDriver driver = GetDriver(2);
+
+		sendCommandViaChat(driver, enterExistingRoomCommand);
+
+		return roomName;
+	}
+
+	public void CriaSalaTest() {
+		int i = 1;
+		WebDriver driver = GetDriver(i);
+		String roomName = CriarSala(i);
+
+		switchDriverToFrameSource(driver, "messages");
+		assertTrue(driver.getPageSource().indexOf("You just created " + roomName + ".") > 0);
+		switchDriverBackToDefault(driver);
+		closeDrivers();
+	}
+
+	public void EntrarSalaTest() {
+		WebDriver driver1 = GetDriver(1), driver = GetDriver(2);
+		String roomName = EntrarSala();
+
+		switchDriverToFrameSource(driver, "messages");
+		assertTrue(driver.getPageSource().indexOf("You have just joined " + roomName + ".") > 0);
+		switchDriverBackToDefault(driver);
+		closeDrivers();
+	}
+
+	public void sendCommandViaChat(WebDriver driver, String command) {
+		driver.findElement(By.cssSelector("input[name=\"message\"]")).sendKeys(command);
+		driver.findElement(By.cssSelector("input[name=\"message\"]")).sendKeys(Keys.ENTER);
+	}
+
+	private void switchDriverToFrameSource(WebDriver driver, String frameName) {
+		WebElement fr = driver.findElement(By.name(frameName));
+		driver.switchTo().frame(fr);
+	}
+
+	private void switchDriverBackToDefault(WebDriver driver) {
+		driver.switchTo().defaultContent();
+	}
+
+	private void closeDrivers(){
+		if(this.chrome1 != null){
+			this.chrome1.close();
+		}
 		
+		if(this.chrome2 != null) {
+			this.chrome2.close();
+		}
 	}
 }
