3fd6d5a61e187bd3e3ddaad8ba4f2b034c8516d3
Vinicius Frederico <vafrederico@gmail.com>
2017-07-02 16:35:15 -0300

    junit

---
+++ src/teste/CSChatTest.java
@@ -0,0 +1,188 @@
+package teste;
+
+import java.io.File;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+public class CSChatTest {
+
+	private static WebDriver chrome1;
+	private static WebDriver chrome2;
+
+	private static String URL = "http://127.0.0.1:1976/";
+
+	private static void StartChrome1() {
+		if (chrome1 != null)
+			return;
+		chrome1 = new ChromeDriver();
+		chrome1.get(URL);
+	}
+
+	private static void StartChrome2() {
+		if (chrome2 != null)
+			return;
+		chrome2 = new ChromeDriver();
+		chrome2.get(URL);
+	}
+
+	@BeforeClass
+	public static void beforeClass() {
+		File classpathRoot = new File(System.getProperty("user.dir"));
+        File chromedriver = new File(classpathRoot, "driver/chromedriver");
+        System.setProperty("webdriver.chrome.driver", chromedriver.getAbsolutePath());
+		StartChrome1();
+	}
+
+	private static WebDriver GetDriver(int ind) {
+		switch (ind) {
+			case 1:
+				StartChrome1();
+				return chrome1;
+			case 2:
+				StartChrome2();
+				return chrome2;
+			default:
+				StartChrome1();
+				return chrome1;
+		}
+	}
+
+	private  void Login(int ind) {
+        Calendar cal = Calendar.getInstance();
+		String username = "usuario" + cal.getTimeInMillis();
+		String password = "senha";
+
+		WebDriver driver = GetDriver(ind);
+
+		driver.findElement(By.cssSelector("input[name=\"username\"]")).sendKeys(username);
+		driver.findElement(By.cssSelector("input[name=\"password\"]")).sendKeys(password);
+        driver.findElement(By.cssSelector("input[value=\"[login]\"]")).click();
+    }
+
+	private  void Mensagem(int ind) {
+		WebDriver driver = GetDriver(ind);
+		Login(ind);
+		Login(ind);
+	}
+
+	private String CriarSala(int ind) {
+	    Calendar cal = Calendar.getInstance();
+		String uniqueRoomName = "Room" + cal.getTimeInMillis();
+		String createRoomCommand = "/j " + uniqueRoomName;
+		WebDriver driver = GetDriver(ind);
+
+        Login(ind);
+        driver.navigate().to(URL + "/LOGIN");
+		sendCommandViaChat(driver, createRoomCommand);
+
+		return uniqueRoomName;
+	}
+
+	private  String EntrarSala() {
+		Login(1);
+		Login(2);
+		String roomName = CriarSala(1);
+		String enterExistingRoomCommand = "/j " + roomName;
+		WebDriver driver = GetDriver(2);
+
+		sendCommandViaChat(driver, enterExistingRoomCommand);
+
+		return roomName;
+	}
+
+	@Test
+	public void CriaSalaTest() {
+		int i = 1;
+		WebDriver driver = GetDriver(i);
+		String roomName = CriarSala(i);
+
+		switchDriverToFrameSource(driver, "messages");
+		assertTrue(driver.getPageSource().indexOf("You just created " + roomName + ".") > 0);
+		closeDrivers();
+	}
+
+    @Test
+    public void LoginTest() {
+        int i = 1;
+        WebDriver driver = GetDriver(i);
+        Login(1);
+
+        switchDriverToFrameSource(driver, "messages");
+        assertTrue(driver.getPageSource().indexOf("Please obey the netiquette.") > 0);
+        closeDrivers();
+    }
+
+	@Test
+	public void EnviarMensagemTest() {
+		WebDriver driver = GetDriver(1);
+		Login(1);
+
+		sendCommandViaChat(driver, "Oi!");
+		switchDriverToFrameSource(driver, "messages");
+		assertTrue(driver.getPageSource().indexOf("Oi!") > 0);
+		closeDrivers();
+	}
+
+
+    @Test
+    public void EnviarShoutTest() {
+        WebDriver driver = GetDriver(1);
+        Login(1);
+
+        sendCommandViaChat(driver, "/s Oi!");
+        switchDriverToFrameSource(driver, "messages");
+        assertTrue(driver.getPageSource().indexOf("shouts out OI!") > 0);
+        closeDrivers();
+    }
+
+
+    @Test
+    public void EntrarSalaTest() {
+        WebDriver driver1 = GetDriver(1), driver = GetDriver(2);
+        String roomName = EntrarSala();
+
+        switchDriverToFrameSource(driver, "messages");
+        assertTrue(driver.getPageSource().indexOf("You have just joined " + roomName + ".") > 0);
+        switchDriverBackToDefault(driver);
+        closeDrivers();
+    }
+
+    private  void sendCommandViaChat(WebDriver driver, String command) {
+        switchDriverToFrameSource(driver, "input");
+	    driver.findElement(By.name("message")).sendKeys(command);
+		driver.findElement(By.name("message")).sendKeys(Keys.ENTER);
+	}
+
+	private void switchDriverToFrameSource(WebDriver driver, String frameName) {
+        driver.switchTo().defaultContent();
+		WebElement fr = driver.findElement(By.name(frameName));
+		driver.switchTo().frame(fr);
+	}
+
+	private void switchDriverBackToDefault(WebDriver driver) {
+		driver.switchTo().defaultContent();
+	}
+
+	@AfterClass
+	public static void closeDrivers(){
+		if(chrome1 != null){
+			chrome1.close();
+		}
+
+		if(chrome2 != null) {
+			chrome2.close();
+		}
+	}
+}
